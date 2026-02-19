cf03a45dae1c32dce960dd1f55e4826dd7a092fb
BrunoNguyen2905 <nhannguyen290520@gmail.com>
2026-02-19 15:05:26 +0200

    add domain to Allowed hosts in settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SOUNDSTRIPE_API_KEY_DEVELOPMENT = env.str("SOUNDSTRIPE_API_KEY_DEVELOPMENT", "")
 DEBUG = True
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "django-projects-jubb.onrender.com"]
 
 
 # Application definition
